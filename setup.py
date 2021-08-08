6113ce45d5effa4a53245d04a3c33f23c3aed82d
David-Leatham <david@leatham.de>
2021-08-08 13:58:07 +0200

    Let's go on an adventure!

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, Extension
+
+
+setup(
+    name='licentra',
+    version='0.0.4',
+    description='Python Package with Hello World C Extension',
+    ext_modules=[
+        Extension(
+            'licentra',
+            sources=['licentra.c'],
+            py_limited_api=True)
+    ],
+)
